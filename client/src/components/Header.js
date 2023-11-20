c4065f6a7358951594a00e3b9f482b1d58b1df96
HemantWD <hemantajaysharma@gmail.com>
2023-11-20 13:32:36 +0530

    frontend

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className=" bg-slate-200 flex justify-center items-center leading-10 p-5">
+      <img
+        src="https://assets-global.website-files.com/62f24893562f47d8766501aa/62f24ef3fde2c2f124a46baf_teachforindia_logo.svg"
+        alt="Logo"
+      />
+    </div>
+  );
+};
+
+export default Header;
